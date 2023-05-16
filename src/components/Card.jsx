79d54fc01845123fcb02c0394165dbbf5f5d0df9
CarlMBergman <113527185+CarlMBergman@users.noreply.github.com>
2023-05-16 10:20:08 +0200

    få bilder att funka

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,36 @@
 import './Card.scss'
 import { setActive, removeCard, localStorageUpdate } from '../redux/action';
 import { useDispatch } from 'react-redux';
-import chip from '../assets/chip-dark.svg'
+import chip        from '../assets/chip-dark.svg';
+import bitcoinV    from '../assets/vendor-bitcoin.svg';
+import blockchainV from '../assets/vendor-blockchain.svg';
+import evilV       from '../assets/vendor-evil.svg';
+import ninjaV      from '../assets/vendor-ninja.svg';
+import XV          from '../assets/transparentX.svg';
+import { useEffect, useState } from 'react';
 
 function Card(props) {
     const dispatch = useDispatch()
+    const [vendor, setVendor] = useState()
+
+    function testVendor() {
+        if (props.fullCard.vendor === 'bitcoin') {
+            setVendor(bitcoinV)
+        }
+        else if (props.fullCard.vendor === 'ninja') {
+            setVendor(ninjaV)
+        }
+        else if (props.fullCard.vendor === 'blockchain') {
+            setVendor(blockchainV)
+        }
+        else if (props.fullCard.vendor === 'evil') {
+            setVendor(evilV)
+        }
+    }
+
+    useEffect(() => {
+        testVendor()
+    }, [])
 
     function handleActive() {
         if (props.fullCard.isActive === 'notActive') {
@@ -26,7 +52,7 @@ function Card(props) {
         <article className={`card ${props.fullCard.vendor} ${props.fullCard.isActive}`} onClick={ handleActive }>
             <div className='card__vendor'>
                 <img src={ chip } alt="chip" />
-                <img className='card__vendor-vendor' src={`/src/assets/vendor-${props.fullCard.vendor}.svg`} alt="vendor" />
+                <img className='card__vendor-vendor' src={ vendor } alt="vendor" />
             </div>
             <p className='card__num'>{ props.fullCard.cardNum }</p>
             <div className='card__info'>
@@ -39,7 +65,7 @@ function Card(props) {
                     <p>{ props.fullCard.valid }</p>
                 </div>
             </div>
-            <img className='card__removebtn' onClick={ handleRemove } src="/src/assets/transparentX.svg" alt="removebtn" />
+            <img className='card__removebtn' onClick={ handleRemove } src={ XV } alt="removebtn" />
         </article>
         
         
